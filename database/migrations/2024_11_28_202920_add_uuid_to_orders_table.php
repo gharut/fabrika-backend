210c206b3b7d8ffeb9782c3e1e62a5cca3353022
Hovsep Miskaryan <miskaryan64@gmail.com>
2024-11-29 01:46:03 +0400

    Migration fix

--- database/migrations/2024_11_28_202920_add_uuid_to_orders_table.php
+++ database/migrations/2024_11_28_202920_add_uuid_to_orders_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->uuid()->default(DB::raw('UUID()'))->after('id');
+            $table->uuid()->after('id');
         });
     }
 
