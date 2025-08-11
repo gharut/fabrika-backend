981e05742d0a77b332f11135d8937da2c5aa13a2
GanievIbraim <ganiev.i.i.21@gmail.com>
2025-08-11 21:55:45 +0300

    Тест

--- app/Http/Controllers/Api/WbController.php
+++ app/Http/Controllers/Api/WbController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use App\Services\Wb\WbService;
+use App\Services\WB\WbService;
 
 class WbController extends Controller
 {
