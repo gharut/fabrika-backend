ba5b3731933c0f35fbd693e6ea9cb39518063986
GanievIbraim <ganiev.i.i.21@gmail.com>
2025-09-13 15:24:43 +0300

    Fix DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,6 @@ class DatabaseSeeder extends Seeder
             PermissionSeeder::class,
             LabelTemplateSeeder::class,
             RolesSeeder::class,
-            UsersSeeder::class,
         ]);
     }
 }
