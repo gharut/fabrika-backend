a5ae4000de0c25d65d06bb0cd98d9bbf8f36023d
GanievIbraim <ganiev.i.i.21@gmail.com>
2025-10-26 18:16:45 +0300

    Fix sycnWbData

--- app/Console/Commands/SyncWbData.php
+++ app/Console/Commands/SyncWbData.php
@@ -29,10 +29,15 @@ class SyncWbData extends Command
                 $countPrice = $syncProductPrice->syncPrice($account->api_token_enc);
 
                 $syncInventoryLevel = app(InventoryLevelSyncService::class);
-                $countStock = $syncInventoryLevel->syncStock($account->id);
+                $resultSyncStock = $syncInventoryLevel->syncStock($account->id);
+                $syncStockSuccess = $resultSyncStock->success;
+                $syncStockMessage = $resultSyncStock->message;
 
-                Log::info("[WB Sync] Аккаунт {$account->id}: Обновлено цен {$countPrice}, остатков {$countStock}");
+                $statusText = $syncStockSuccess
+                    ? 'успешно'
+                    : "ошибка: {$syncStockMessage}";
 
+                Log::info("[WB Sync] Аккаунт {$account->id}: Обновлено цен {$countPrice}, обновление остатков {$statusText}");
             } catch (\Throwable $e) {
                 Log::error("[WB Sync] Ошибка для аккаунта {$account->id}: {$e->getMessage()}");
             }
